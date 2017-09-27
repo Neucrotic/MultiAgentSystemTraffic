a81c6c93a2fec9d1c749dd835dd7137a02d68b7b
101533031 <101533031@ATC620-04.ds.swin.edu.au>
2017-09-27 16:04:36 +1000

    Added program loop

--- MultiAgentTrafficSystem/src/Main.java
+++ MultiAgentTrafficSystem/src/Main.java
@@ -5,6 +5,12 @@ public class Main
 	public static void main(String[] args)
 	{
 		Network network = new Network(5);
+		
+		while (network.run)
+		{
+			network.Update();
+			network.Draw();
+		}
 	}
 
 }
--- MultiAgentTrafficSystem/src/Network.java
+++ MultiAgentTrafficSystem/src/Network.java
@@ -1,9 +1,20 @@
 public class Network
 {
-	int vehicleCount = 0;
+	public boolean run = true;
+	private int vehicleCount = 0;
 	
 	Network(int vehicleCount)
 	{
 		this.vehicleCount = vehicleCount;
+	}
+	
+	public void Update()
+	{
+		
+	}
+	
+	public void Draw()
+	{
+		
 	}
 }
